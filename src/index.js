e7be21d1a6ca9396f75f78763928a76b4775725a
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-09-15 09:00:12 -0300

    First http method 'GET'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/",(req,res) => {
+    return res.send("Hello");
+})
+
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
 });
